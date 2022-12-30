8f4c5ddb6099f28a37a3364ab6a04886f311b27b
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2022-12-30 13:57:58 -0600

    Added commands give and list items, also added item IDs

--- src/main/java/me/mythicalflame/spigotmodding/commands/CommandSpigotModding.java
+++ src/main/java/me/mythicalflame/spigotmodding/commands/CommandSpigotModding.java
@@ -1,7 +1,8 @@
 package me.mythicalflame.spigotmodding.commands;
 
+import me.mythicalflame.spigotmodding.SpigotModding;
 import me.mythicalflame.spigotmodding.items.ModdedItem;
-import me.mythicalflame.spigotmodding.items.ModdedItemTinSword;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,10 +13,82 @@ public class CommandSpigotModding implements CommandExecutor
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        //TODO: give command
-        /*Player player = (Player) sender;
-        ModdedItem testItem = new ModdedItemTinSword();
-        player.getInventory().addItem(testItem.getItem());*/
+        if (args.length == 0)
+        {
+            helpMessage(sender);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("give"))
+        {
+            if (args.length <= 2)
+            {
+                sender.sendMessage(ChatColor.DARK_GREEN + "/spigotmodding give <itemNamespace> <itemID>");
+                return true;
+            }
+            //check namespace existence
+            boolean namespaceExists = false;
+            for (ModdedItem item : SpigotModding.registeredItems)
+            {
+                if (args[1].equalsIgnoreCase(item.getNamespace()))
+                {
+                    namespaceExists = true;
+                    break;
+                }
+            }
+
+            if (namespaceExists)
+            {
+                ModdedItem receivingItem = null;
+                //check if item with namespace exists
+                boolean exactItemExists = false;
+                for (ModdedItem item : SpigotModding.registeredItems)
+                {
+                    if (args[1].equalsIgnoreCase(item.getNamespace()) && args[2].equalsIgnoreCase(item.getID()))
+                    {
+                        receivingItem = item;
+                        exactItemExists = true;
+                        break;
+                    }
+                }
+
+                if (exactItemExists)
+                {
+                    //give requested item
+                    Player player = (Player) sender;
+                    player.getInventory().addItem(receivingItem.getItem());
+                }
+                else
+                {
+                    sender.sendMessage(ChatColor.RED + "Could not find item " + args[1] + ":" + args[2] + "!");
+                    sender.sendMessage(ChatColor.DARK_GREEN + "/spigotmodding give <itemNamespace> <itemID>");
+                }
+            }
+            else
+            {
+                sender.sendMessage(ChatColor.RED + "Could not find namespace " + args[1] + "!");
+                sender.sendMessage(ChatColor.DARK_GREEN + "/spigotmodding give <itemNamespace> <itemID>");
+            }
+        }
+        else if (args[0].equalsIgnoreCase("items"))
+        {
+            String result = "List of registered items: ";
+            for (ModdedItem item : SpigotModding.registeredItems)
+            {
+                result += item.getNamespace() + ":" + item.getID() + ", ";
+            }
+
+            sender.sendMessage(result);
+        }
+        else
+        {
+            helpMessage(sender);
+        }
         return true;
+    }
+
+    public void helpMessage(CommandSender sender)
+    {
+        sender.sendMessage(ChatColor.GOLD + "SpigotModding commands help:\n" + ChatColor.DARK_GREEN + "/spigotmodding help " + ChatColor.RESET + "Displays this message.\n" + ChatColor.DARK_GREEN + "/spigotmodding give " + ChatColor.RESET + "Gives custom features.\n");
     }
 }
