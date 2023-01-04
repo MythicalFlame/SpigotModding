421abf3a980b316139bdb44844df6c1ec4698a08
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2023-01-04 12:28:02 -0600

    Add ModdedConsumable

--- src/main/java/me/mythicalflame/spigotmodding/EventWatcher.java
+++ src/main/java/me/mythicalflame/spigotmodding/EventWatcher.java
@@ -1,19 +1,21 @@
 package me.mythicalflame.spigotmodding;
 
 import me.mythicalflame.spigotmodding.SpigotModding;
+import me.mythicalflame.spigotmodding.items.ModdedConsumable;
 import me.mythicalflame.spigotmodding.items.ModdedItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class EventWatcher implements Listener
 {
     //NOTE: does not do anything if player clicks on a block instead of air. Could be a separate method?
     @EventHandler
-    public void ClickWatcher(PlayerInteractEvent event)
+    public void clickWatcher(PlayerInteractEvent event)
     {
         Player player = event.getPlayer();
         ItemStack hand = player.getItemInHand();
@@ -33,12 +35,34 @@ public class EventWatcher implements Listener
         {
             if (event.getAction() == Action.LEFT_CLICK_AIR)
             {
-                itemUsed.onLeftClick();
+                itemUsed.onLeftClick(event);
             }
             else if (event.getAction() == Action.RIGHT_CLICK_AIR)
             {
-                itemUsed.onRightClick();
+                itemUsed.onRightClick(event);
             }
+        }
+    }
+
+    @EventHandler
+    public void consumeWatcher(PlayerItemConsumeEvent event)
+    {
+        ItemStack consumed = event.getItem();
+        ModdedConsumable consumableUsed = null;
+        boolean consumedModdedConsumable = false;
+
+        for (ModdedConsumable item : SpigotModding.consumables)
+        {
+            if (consumed.equals(item.getItem()))
+            {
+                consumableUsed = item;
+                consumedModdedConsumable = true;
+            }
+        }
+
+        if (consumedModdedConsumable)
+        {
+            consumableUsed.onConsume(event);
         }
     }
 }
