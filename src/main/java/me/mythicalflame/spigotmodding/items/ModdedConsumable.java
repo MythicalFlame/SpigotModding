7884625660e25b37f5d1f52f480b12fdb29ccd0c
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 15:47:35 -0600

    Change item comparison in event watcher to checking if first String in lore is the same. This allows custom items to receive custom lore and be renamed but still have functionality.

--- src/main/java/me/mythicalflame/spigotmodding/items/ModdedConsumable.java
+++ src/main/java/me/mythicalflame/spigotmodding/items/ModdedConsumable.java
@@ -2,19 +2,20 @@ package me.mythicalflame.spigotmodding.items;
 
 import org.bukkit.Material;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.Recipe;
 
 public class ModdedConsumable extends ModdedItem
 {
     //constructor without customModelData
-    public ModdedConsumable(String namespace, String ID, Material material, String name)
+    public ModdedConsumable(String namespace, String ID, Material material, String name, Recipe[] recipes)
     {
-        super(namespace, ID, material, name);
+        super(namespace, ID, material, name, recipes);
     }
 
     //constructor with customModelData
-    public ModdedConsumable(String namespace, String ID, Material material, String name, int customModelData)
+    public ModdedConsumable(String namespace, String ID, Material material, String name, Recipe[] recipes, int customModelData)
     {
-        super(namespace, ID, material, name, customModelData);
+        super(namespace, ID, material, name, recipes, customModelData);
     }
 
     public void onConsume(PlayerItemConsumeEvent event){}
